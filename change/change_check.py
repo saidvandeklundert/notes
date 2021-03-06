bafc773dfc7ec1e691ca816b87243626fb7eb2b2
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-03-06 08:51:16 +0100

    words

--- change/change_check.py
+++ change/change_check.py
@@ -29,11 +29,13 @@ class NodeTester:
         for key in all_keys_set:
             if test_pre_d.get(key) != test_post_d.get(key):
                 diff_d[key] = [ { 'pre' : test_pre_d.get(key) }, {'post' : test_post_d.get(key)}]
+        # record change in nested dictionary:
+
         return diff_d
 
-    def bgp_v4_test(self,):
+    def bgp_test(self,):
         """Execute the bgp_v4_test check"""
-        test_pre_d, test_post_d = self._load_test(test_name='bgp_v4')
+        test_pre_d, test_post_d = self._load_test(test_name='bgp')
         print(test_pre_d.keys())
         print(test_post_d.keys())
         return test_pre_d, test_post_d
@@ -44,6 +46,6 @@ class NodeTester:
 if __name__ == "__main__":
     # debug purposes
     x = NodeTester(hostname = 'router1', path ='C:\\dev-container\\python\\change')
-    x.bgp_v4_test()
-    d1, d2 = x.bgp_v4_test()
+    x.bgp_test()
+    d1, d2 = x.bgp_test()
     x._dict_diff(d1, d2)
