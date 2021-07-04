9043cdd1f1d6ee46e6c522e6dac4f8d359f2aab6
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-04 07:37:08 +0200

    python

--- examples/async_snmp.py
+++ examples/async_snmp.py
@@ -3,21 +3,42 @@ import aiosnmp
 import os
 
 """
-	
 		".1.3.6.1.2.1.1.1.0",
 		"1.3.6.1.2.1.1.5.0",
 """
-
+hosts = [line.rstrip() for line in open("hosts.txt")]
 # export SNMP_STRING=xxx
 SNMP_STRING = os.getenv("SNMP_STRING")
 
 
+async def async_snmp(host):
+    print("async snmp")
+    try:
+        async with aiosnmp.Snmp(
+            host=host, port=161, community=SNMP_STRING, timeout=1
+        ) as snmp:
+            for res in await snmp.get(".1.3.6.1.2.1.1.1.0"):
+                print(res.oid, res.value)
+    except Exception as err:
+        print(f"{err} touching {host}")
+
+
 async def main():
     async with aiosnmp.Snmp(
-        host="10.198.137.68", port=161, community=SNMP_STRING
+        host=hosts[2], port=161, community=SNMP_STRING, timeout=2
     ) as snmp:
         for res in await snmp.get(".1.3.6.1.2.1.1.1.0"):
             print(res.oid, res.value)
+    async with aiosnmp.Snmp(
+        host="1.1.1.1", port=161, community=SNMP_STRING, timeout=1
+    ) as snmp:
+        try:
+            for res in await snmp.get(".1.3.6.1.2.1.1.1.0"):
+                print(res.oid, res.value)
+        except:
+            pass
+    await asyncio.gather(*(async_snmp(host) for host in hosts))
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
