e239a1eee80cfe7d6d211ffcee45bc5a18c15e35
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-06 19:00:17 +0200

    python

--- examples/extending_python/main.go
+++ examples/extending_python/main.go
@@ -1,16 +1,16 @@
 /*
 
-   yum install wget
-   git clone https://github.com/saidvandeklundert/python.git
-   wget https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
-   rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz
-   export PATH=$PATH:/usr/local/go/bin
-   go version
+	yum install wget
+	git clone https://github.com/saidvandeklundert/python.git
+	wget https://golang.org/dl/go1.16.3.linux-amd64.tar.gz
+	rm -rf /usr/local/go && tar -C /usr/local -xzf go1.16.3.linux-amd64.tar.gz
+	export PATH=$PATH:/usr/local/go/bin
+	go version
+	yum install gcc
+	go build -buildmode=c-shared -o main.so main.go
 
-   go build -buildmode=c-shared -o main.so main.go
 
-
-   go_python.py will asume these files are in the same directory.
+	go_python.py will asume these files are in the same directory.
 */
 
 package main
