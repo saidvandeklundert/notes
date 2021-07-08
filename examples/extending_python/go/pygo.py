ed03baa0d55fdd312085f50930d3c599f15e2719
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-07-08 16:54:56 +0200

    python

--- examples/extending_python/go/pygo.py
+++ examples/extending_python/go/pygo.py
@@ -12,18 +12,14 @@ import psutil
 go_lib = cdll.LoadLibrary("./pygo.so")
 go_lib.pygo.restype = c_char_p
 
-string_list = ["aaaa" for x in range(100000)]
 
 args_d = {
     "str": "string",
     "int": 100,
     "float": 1.2,
     "mapping": {"a": "a"},
-    "slice": string_list,
+    "slice": ["a", "b"],
 }
-
-
-string_list = ["aaaa" for x in range(10000000)]
 
 
 def rand_args() -> Dict:
@@ -38,15 +34,17 @@ def rand_args() -> Dict:
     return args_d
 
 
+JSON_STRING = json.dumps(args_d).encode("utf8")
+
+
 if __name__ == "__main__":
     # run the go func once:
-    start = time.time()
-    jsonString = json.dumps(args_d).encode("utf8")
-    end = time.time()
-    print(f"sending JSON string of size {sys.getsizeof(string_list)}: {end - start}")
 
-    go_response = go_lib.pygo(jsonString)
+    start = time.time()
+    go_response = go_lib.pygo(JSON_STRING)
+    end = time.time()
     go_response = go_response.decode()
+    print(f"Calling Go fun took: {end - start}")
     print(go_response)
 
     # run the fun many times and see if how the memory is:
@@ -61,18 +59,13 @@ if __name__ == "__main__":
         print(f"arg has size {sys.getsizeof(arg)}\tmemory usage is {mem_usage}")
 """
 python.\pygo.py
-sending JSON string of size 89095160: 0.009000539779663086
+Calling Go fun took: 0.0034389495849609375
 {"str":"Marie","int":111,"float":0,"slice":["b","b","b","b","b"],"mapping":null}
-arg has size 20697965   memory usage is 312.56640625
-arg has size 15976685   memory usage is 387.03125
-arg has size 8676709    memory usage is 380.0
-arg has size 15009140   memory usage is 385.9921875
-arg has size 6453149    memory usage is 377.78125
-arg has size 21978415   memory usage is 392.01171875
-arg has size 5056237    memory usage is 375.828125
-arg has size 19435622   memory usage is 389.3515625
-arg has size 9077821    memory usage is 379.12890625
-arg has size 15762750   memory usage is 385.2890625
-arg has size 10173398   memory usage is 379.484375
-arg has size 7635814    memory usage is 377.07421875
+arg has size 23604190   memory usage is 241.44921875
+arg has size 2717414    memory usage is 221.76171875
+arg has size 19029990   memory usage is 286.875
+arg has size 8382429    memory usage is 276.4921875
+arg has size 15518502   memory usage is 282.87890625
+arg has size 5286485    memory usage is 272.9609375
+arg has size 11237877   memory usage is 278.50390625
 """
