93ce1c0b21c2cbdbfa633c92b5d9c4b13386a7da
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-10-16 11:39:07 +0200

    doxtest example

--- interesting models and packages/pydantic/base_model_w_doctest.py
+++ interesting models and packages/pydantic/base_model_w_doctest.py
@@ -26,3 +26,32 @@ age: 6
     )
     p = Person(**d)
     print(p.json(indent=2))
+    print(
+        """ To run the doctests:
+python -m doctest -v .\base_model_w_doctest.py
+Trying:
+    from yaml_doctest_input import *
+Expecting nothing
+ok
+Trying:
+    d = safe_load(YAML_EXAMPLE)
+Expecting nothing
+ok
+Trying:
+    p = Person(**d)
+Expecting nothing
+ok
+Trying:
+    assert p.name == "Jan"
+Expecting nothing
+ok
+2 items had no tests:
+    base_model_w_doctest
+    base_model_w_doctest.Person.__config__
+1 items passed all tests:
+   4 tests in base_model_w_doctest.Person
+4 tests in 3 items.
+4 passed and 0 failed.
+Test passed.
+"""
+    )
