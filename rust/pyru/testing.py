8d2dbb3944e022fda9e06de699b06e617c8e4ceb
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-10-31 09:33:17 +0100

    adding pyru

--- rust/pyru/testing.py
+++ rust/pyru/testing.py
@@ -1,7 +1,7 @@
 import ctypes
 
 # following is where the Rust library should be located:
-library_name = "target/release/libstringtools.so"
+library_name = "target/release/pyru.so"
 # loaing the library:
 rust_code = ctypes.CDLL(library_name)
 # Using the library:
