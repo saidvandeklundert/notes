438616c0aaf8d17611c5a553b106517fc80bf719
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-10-31 19:30:10 +0100

    adding

--- rust/pyru/person_from_p_to_r.py
+++ rust/pyru/person_from_p_to_r.py
@@ -1,7 +1,6 @@
-# script that calls a rust function from the pyru library.
-# first part creates a pydantic basemodel class.
-## second part loads the Rust function and sends over the pydantic model.
-"""Part 1"""
+# In the python script, instantiate a 'Person' and send it to a Rust function as bytes
+# On the Rust side, read the bytes as a string, load it as JSON and Deserialize it into a Person struct.
+"""Part 1: setting up the Python person class:"""
 from pydantic import BaseModel
 
 
@@ -14,7 +13,8 @@ jan = Person(name="Jan", age=6)
 
 # output the model as JSON string, then convert to bytes encoded a utf-8:
 json_json_str = jan.json(indent=2).encode("utf-8")
-"""Part 2"""
+
+"""Part 2: Load the library that enables using the Rust functions in Python:"""
 import ctypes
 
 # following is where the Rust library should be located:
@@ -22,6 +22,13 @@ library_name = "target/release/libpyru.so"
 # loaing the library:
 pyru = ctypes.CDLL(library_name)
 
-
 # Using the library (send a string as bytes that are encoded as utf-8):
 pyru.python_person_to_rust(json_json_str)
+
+if __name__ == "__main__":
+    import sys
+
+    # Make infinite function calls and monitor mem usage
+    if len(sys.argv) > 1:
+        while True:
+            pyru.python_person_to_rust(json_json_str)
