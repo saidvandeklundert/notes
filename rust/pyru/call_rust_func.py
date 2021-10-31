adbfcfef437aaf699d928d119e52c55b2a755326
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-10-31 09:45:06 +0100

    libpyru

---
+++ rust/pyru/call_rust_func.py
@@ -0,0 +1,26 @@
+# script that calls a rust function from the pyru library.
+# first part creates a pydantic basemodel class.
+## second part loads the Rust function and sends over the pydantic model.
+"""Part 1"""
+from pydantic import BaseModel
+
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+
+jan = Person(name="Jan", age=6)
+
+json_json_str = jan.json(indent=2).encode("utf-8")
+"""Part 2"""
+import ctypes
+
+# following is where the Rust library should be located:
+library_name = "target/release/libpyru.so"
+# loaing the library:
+pyru = ctypes.CDLL(library_name)
+
+
+# Using the library:
+print(pyru.python_to_rust(json_json_str, b"na"))
--- rust/pyru/testing.py
+++
@@ -1,8 +0,0 @@
-import ctypes
-
-# following is where the Rust library should be located:
-library_name = "target/release/libpyru.so"
-# loaing the library:
-rust_code = ctypes.CDLL(library_name)
-# Using the library:
-print(rust_code.python_to_rust(b"banana", b"na"))
