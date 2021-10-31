4e3b0a1525498c3f258b7ebe3dc356c58615ced8
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-10-31 09:32:59 +0100

    adding pyru

---
+++ rust/pyru/src/lib.rs
@@ -0,0 +1,13 @@
+extern crate libc;
+
+use libc::c_char;
+use std::ffi::CStr;
+use std::str;
+
+/// Receive data from the Python universe and use it in Rust.
+#[no_mangle]
+pub extern "C" fn python_to_rust(value: *const c_char, substr: *const c_char) -> i32 {
+    let c_value = unsafe { CStr::from_ptr(value).to_bytes() };
+    let python_string = str::from_utf8(c_value);
+    println!("Python string: {}", python_string.unwrap());
+}
--- rust/pyru/testing.py
+++ rust/pyru/testing.py
@@ -3,6 +3,6 @@ import ctypes
 # following is where the Rust library should be located:
 library_name = "target/release/libstringtools.so"
 # loaing the library:
-stringtools = ctypes.CDLL(library_name)
+rust_code = ctypes.CDLL(library_name)
 # Using the library:
-print(stringtools.count_substrings(b"banana", b"na"))
+print(rust_code.python_to_rust(b"banana", b"na"))
