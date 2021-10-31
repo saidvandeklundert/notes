0a713cf13c38daa3d87cfd6cf3071a7987f76f59
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-10-31 11:15:50 +0100

    adding

--- rust/pyru/call_rust_func.py
+++ rust/pyru/call_rust_func.py
@@ -29,7 +29,9 @@ rust_return = pyru.python_to_rust(json_json_str)
 import ctypes
 
 rust_return_bytes = ctypes.c_char_p(rust_return)
-rust_return_string = (
-    rust_return_bytes.value.decode("utf-8") if rust_return_bytes.value else None
-)
+rust_return_string = rust_return_bytes.value
+
+if rust_return_bytes:
+    rust_return_bytes = rust_return_bytes.decode("utf-8")
+
 print(rust_return_string)
