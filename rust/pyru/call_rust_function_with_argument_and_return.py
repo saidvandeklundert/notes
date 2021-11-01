f4e5e3d6518435302fa156b44602688bdf3de98a
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-01 13:47:45 +0100

    adding

---
+++ rust/pyru/call_rust_function_with_argument_and_return.py
@@ -0,0 +1,22 @@
+import ctypes
+from pydantic import BaseModel
+
+
+class Person(BaseModel):
+    name: str
+    age: int
+
+
+pyru = ctypes.CDLL("target/release/libpyru.so")
+
+if __name__ == "__main__":
+    marie = Person(name="Marie", age=2)
+    marie_json_string = marie.json(indent=2).encode("utf-8")
+
+    rust_return = pyru.python_to_rust(marie_json_string)
+    # Read the return from Rust as a string:
+    rust_return_string = ctypes.c_char_p(rust_return).value
+    print(f"Rust returned the following:\n{rust_return_string}")
+
+    # free the memory allocated by Rust:
+    pyru.free_rust_string(rust_return)
