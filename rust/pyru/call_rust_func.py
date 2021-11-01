913b4e1d546ec8be824231b3b914f162bf887178
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-01 13:53:23 +0100

    adding

--- rust/pyru/call_rust_func.py
+++ rust/pyru/call_rust_func.py
@@ -3,6 +3,7 @@
 ## second part loads the Rust function and sends over the pydantic model.
 """Part 1"""
 from pydantic import BaseModel
+import ctypes
 
 
 class Person(BaseModel):
@@ -10,54 +11,33 @@ class Person(BaseModel):
     age: int
 
 
-jan = Person(name="Jan", age=6)
-
-# output Person instance as JSON string, then convert it to bytes, encoded as utf-8:
-json_json_str = jan.json(indent=2).encode("utf-8")
-"""Part 2"""
-import ctypes
-
-# following is where the Rust library should be located:
 library_name = "target/release/libpyru.so"
-# loading the library:
+
 pyru = ctypes.CDLL(library_name)
 
+if __name__ == "__main__":
+    i = 0
+    loops = 1000000
+    while i < loops:
+        marie = Person(name="Marie", age=2)
+        marie_json_str = marie.json(indent=2).encode("utf-8")
+        rust_return_marie = pyru.python_to_rust(marie_json_str)
+        rust_return_bytes = ctypes.c_char_p(rust_return_marie)
+        rust_return_string = rust_return_bytes.value
 
-# Using the library (send a string as bytes that are encoded as utf-8):
-rust_return = pyru.python_to_rust(json_json_str)
+        if rust_return_string:
+            rust_return_string = rust_return_string.decode("utf-8")
 
-# take the return value and convert it to a string:
-import ctypes
+        if i % 10000 == 0:
+            print(f"Rust return: {rust_return_string}")
+            percent_done = str(int(i / loops * 100))
+            print(f"percent done: {percent_done}")
+        pyru.free_rust_mem(rust_return_marie)
+        marie, rust_return, rust_return_bytes, rust_return_marie = (
+            None,
+            None,
+            None,
+            None,
+        )
 
-rust_return_bytes = ctypes.c_char_p(rust_return)
-rust_return_string = rust_return_bytes.value
-
-if rust_return_string:
-    rust_return_string = rust_return_string.decode("utf-8")
-
-print(rust_return_string)
-print(f"Rust return: {rust_return}")
-# Using the library (send a string as bytes that are encoded as utf-8):
-pyru.free_rust_mem(rust_return)
-
-rust_return = None
-i = 0
-loops = 1000000
-while i < loops:
-    marie = Person(name="Marie", age=2)
-    marie_json_str = marie.json(indent=2).encode("utf-8")
-    rust_return_marie = pyru.python_to_rust(marie_json_str)
-    rust_return_bytes = ctypes.c_char_p(rust_return_marie)
-    rust_return_string = rust_return_bytes.value
-
-    if rust_return_string:
-        rust_return_string = rust_return_string.decode("utf-8")
-
-    if i % 10000 == 0:
-        print(f"Rust return: {rust_return_string}")
-        percent_done = str(int(i / loops * 100))
-        print(f"percent done: {percent_done}")
-    pyru.free_rust_mem(rust_return_marie)
-    marie, rust_return, rust_return_bytes, rust_return_marie = None, None, None, None
-
-    i += 1
+        i += 1
