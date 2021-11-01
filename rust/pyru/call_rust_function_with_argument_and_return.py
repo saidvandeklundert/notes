41cdcaf2ca3e2dbe2936be0fe82e465dc903f9e9
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-01 13:49:41 +0100

    adding

--- rust/pyru/call_rust_function_with_argument_and_return.py
+++ rust/pyru/call_rust_function_with_argument_and_return.py
@@ -16,7 +16,9 @@ if __name__ == "__main__":
     rust_return = pyru.python_to_rust(marie_json_string)
     # Read the return from Rust as a string:
     rust_return_string = ctypes.c_char_p(rust_return).value
-    print(f"Rust returned the following:\n{rust_return_string}")
+    if rust_return_string:
+        rust_return_string = rust_return_string.decode("utf-8")
+        print(f"Rust returned the following:\n{rust_return_string}")
 
     # free the memory allocated by Rust:
     pyru.free_rust_mem(rust_return)
