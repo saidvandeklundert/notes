0f7a03d207a508b26556f9ee42156a8d44402f4b
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-02 20:05:18 +0100

    add new lib

--- rust/rust_lib/print_number.py
+++ rust/rust_lib/print_number.py
@@ -9,5 +9,11 @@ if __name__ == "__main__":
     SOME_BYTES = (100).to_bytes(4, byteorder="little")
     rust_lib.print_int(SOME_BYTES)
 
+    SOME_MORE_BYTES = (2147483646).to_bytes(4, byteorder="little")
+    rust_lib.print_int(SOME_MORE_BYTES)
+
+    SOME_MORE_BYTES = (2147483647).to_bytes(4, byteorder="little")
+    rust_lib.print_int(SOME_MORE_BYTES)
+
     SOME_MORE_BYTES = (2147483648).to_bytes(4, byteorder="little")
     rust_lib.print_int(SOME_MORE_BYTES)
