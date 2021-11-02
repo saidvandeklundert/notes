ea00646b4d8b79bb1c6c466a927c3f0180a66d7b
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-02 19:05:33 +0100

    add new lib

--- rust/rust_lib/print_number.py
+++ rust/rust_lib/print_number.py
@@ -3,5 +3,8 @@ import ctypes
 rust_lib = ctypes.CDLL("target/release/librust_lib.so")
 
 if __name__ == "__main__":
-    SOME_BYTES = (1231254687).to_bytes(4, byteorder="little")
+    SOME_BYTES = (2147483647).to_bytes(4, byteorder="little")
+    rust_lib.print_int(SOME_BYTES)
+
+    SOME_BYTES = (2147483648).to_bytes(4, byteorder="little")
     rust_lib.print_int(SOME_BYTES)
