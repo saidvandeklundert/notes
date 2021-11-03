f45c6ee5b8371f079f8a5921fd56cabae36c2665
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-03 09:53:31 +0100

    add new lib

--- rust/rust_lib2/src/lib.rs
+++ rust/rust_lib2/src/lib.rs
@@ -4,7 +4,6 @@ extern crate serde_json;
 
 use libc::c_char;
 use serde::{Deserialize, Serialize};
-use serde_json::json;
 use std::ffi::CStr;
 use std::ffi::CString;
 use std::str;
@@ -29,12 +28,7 @@ pub extern "C" fn start_procedure(c_string_ptr: *const c_char) -> *mut c_char {
     let bytes = unsafe { CStr::from_ptr(c_string_ptr).to_bytes() };
     let string = str::from_utf8(bytes).unwrap();
     let model: PythonModel = serde_json::from_str(string).unwrap();
-    println!("Taking action using following input:\n{:?}", model);
-    let result = RustResult {
-        result: "success".to_string(),
-        message: "1 host failed".to_string(),
-        failed_hosts: vec!["server1".to_string()],
-    };
+    let result = long_running_task(model);
     let result_json = serde_json::to_string(&result).unwrap();
     let c_string = CString::new(result_json).unwrap();
     c_string.into_raw()
@@ -45,4 +39,14 @@ pub extern "C" fn free_string(c_string_ptr: *mut c_char) {
     unsafe {
         CString::from_raw(c_string_ptr);
     }
+}
+
+fn long_running_task(model: PythonModel) -> RustResult {
+    println!("Taking action using following input:\n{:?}", model);
+    let result = RustResult {
+        result: "success".to_string(),
+        message: "1 host failed".to_string(),
+        failed_hosts: vec!["server1".to_string()],
+    };
+    return result;
 }
