ad7c975821465633b3e27723dcc66ddd4b9bb745
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-03 19:59:24 +0100

    add new lib

---
+++ rust/rust_lib2/call_rust_function_indefinately.py
@@ -0,0 +1,36 @@
+import ctypes
+from pydantic import BaseModel
+from typing import List
+
+rust = ctypes.CDLL("target/release/librust_lib2.so")
+
+
+class PythonModel(BaseModel):
+    timeout: int
+    retries: int
+    host_list: List[str]
+    action: str
+
+
+class RustResult(BaseModel):
+    result: str
+    message: str
+    failed_hosts: List[str]
+
+
+if __name__ == "__main__":
+    i = 1_000_000
+    while i > 0:
+        model = PythonModel(
+            timeout=10, retries=3, action="reboot", host_list=["server1", "server2"]
+        )
+        some_bytes = model.json().encode("utf-8")
+
+        ptr = rust.start_procedure(some_bytes)
+
+        returned_bytes = ctypes.c_char_p(ptr).value
+
+        if returned_bytes:
+            returned_model = RustResult.parse_raw(returned_bytes)
+
+        rust.free_string(ptr)
