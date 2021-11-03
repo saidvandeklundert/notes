31db5057b4797e24093de213181566c473118f52
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-03 08:46:59 +0100

    add new lib

--- rust/rust_lib/print_string_reversed.py
+++ rust/rust_lib/print_string_reversed.py
@@ -1,8 +1,12 @@
 import ctypes
+from sys import argv
 
 rust = ctypes.CDLL("target/release/librust_lib.so")
 
 if __name__ == "__main__":
+    if len(argv) != 2:
+        print("Usage: ./print_string_reversed.py <string>")
+        exit(1)
     rust_return_ptr = rust.reverse_string("Hello, World!".encode("utf-8"))
     rust_return_bytes = ctypes.c_char_p(rust_return_ptr).value
     if rust_return_bytes:
