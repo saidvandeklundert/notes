1e317693bdf406605227f13f67c1f3b86b89fad5
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-03 10:35:22 +0100

    add new lib

--- rust/rust_lib/print_string.py
+++ rust/rust_lib/print_string.py
@@ -7,4 +7,6 @@ if __name__ == "__main__":
     SOME_BYTES = "Python says hi inside Rust!".encode("utf-8")
     rust.print_string(SOME_BYTES)
 
-    rust.print_string(ctypes.c_char_p("Another way of sending strings to Rust via C."))
+    rust.print_string(
+        ctypes.c_char_p("Another way of sending strings to Rust via C.".encode("utf-8"))
+    )
