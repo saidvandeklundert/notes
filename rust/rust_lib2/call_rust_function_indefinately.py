b8617ac965e0394765088801704c33bf20258d25
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-03 20:16:07 +0100

    add new lib

--- rust/rust_lib2/call_rust_function_indefinately.py
+++ rust/rust_lib2/call_rust_function_indefinately.py
@@ -35,6 +35,7 @@ if __name__ == "__main__":
         some_bytes = model.json().encode("utf-8")
 
         ptr = rust.start_procedure(some_bytes)
+        print(ptr)
 
         returned_bytes = ctypes.c_char_p(ptr).value
 
