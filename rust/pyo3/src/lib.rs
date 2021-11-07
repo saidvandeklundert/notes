dab0fafc204ba87b5ba8d01eec7d7d9ea76c755e
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-07 21:09:44 +0100

    stuffz

---
+++ rust/pyo3/src/lib.rs
@@ -0,0 +1,34 @@
+use pyo3::prelude::*;
+
+/// Multiply two numbers:
+#[pyfunction]
+fn multiply(a: isize, b: isize) -> PyResult<isize> {
+    Ok(a * b)
+}
+
+/// Return the sum of a list/vector of numbers
+#[pyfunction]
+fn list_sum(a: Vec<isize>) -> PyResult<isize> {
+    let mut sum: isize = 0;
+    for i in a {
+        sum += i;
+    }
+    Ok(sum)
+}
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust. The name of this function must match
+/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
+/// import the module.
+#[pymodule]
+fn pyo3_examples(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(multiply, m)?)?;
+    m.add_function(wrap_pyfunction!(list_sum, m)?)?;
+    Ok(())
+}
