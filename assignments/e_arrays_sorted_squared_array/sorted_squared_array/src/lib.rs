b43d9b4bd3dc05e8ec175a935a1d05eb23abd360
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-08 11:22:22 +0100

    adding

--- assignments/e_arrays_sorted_squared_array/sorted_squared_array/src/lib.rs
+++ assignments/e_arrays_sorted_squared_array/sorted_squared_array/src/lib.rs
@@ -19,8 +19,8 @@ fn sorted_squared_array_optimized(vec: Vec<isize>) -> PyResult<Vec<isize>> {
     let mut start_index = 0;
     let mut end_index = vec.len() - 1;
 
-    for (idx, _) in vec.iter().enumerate().rev() {
-        //println!("{} {}", idx, value);
+    for (idx, value) in vec.iter().enumerate().rev() {
+        println!("{} {}", idx, value);
         let start_v = vec[start_index];
         let end_v = vec[end_index];
         if start_v.abs() > end_v.abs() {
