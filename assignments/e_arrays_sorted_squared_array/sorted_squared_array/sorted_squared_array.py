8e7acf7ed02b3f6f48ef0e9c44131aef501286d7
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-08 06:16:53 +0100

    adding

--- assignments/e_arrays_sorted_squared_array/sorted_squared_array/sorted_squared_array.py
+++ assignments/e_arrays_sorted_squared_array/sorted_squared_array/sorted_squared_array.py
@@ -16,7 +16,25 @@ def sortedSquaredArray(array):
     return new_array
 
 
+def sorted_squared_array_optimized(array):
+    new_array = []
+    start = 0
+    end = len(array) - 1
+
+    while end > start:
+        if end > start:
+            new_array.append(array[end] ** 2)
+            end -= 1
+        elif start > end:
+            new_array.append(array[start] ** 2)
+            start += 1
+        elif start == end:
+            new_array.append(array[start] ** 2)
+            return new_array
+
+
 if __name__ == "__main__":
     for test in inputs:
-        print("Python", sortedSquaredArray(test))
-        print("Rust", ssa.sorted_squared_array(test))
+        print("Python:", sortedSquaredArray(test))
+        print("Rust:", ssa.sorted_squared_array(test))
+        print("Python optimized:", sorted_squared_array_optimized(test))
