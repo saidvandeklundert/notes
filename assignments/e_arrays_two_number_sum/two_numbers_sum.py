34b001466fcac2f004f651bcc7e2e135719191a6
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-09 06:31:36 +0100

    adding

--- assignments/e_arrays_two_number_sum/two_numbers_sum.py
+++ assignments/e_arrays_two_number_sum/two_numbers_sum.py
@@ -29,7 +29,7 @@ def twoNumberSum_slow(array, targetSum):
 # O(n) space (we create a hashtable)
 def twoNumberSum(array, targetSum):
     seen = {}
-    seen[array.pop()] = True
+
     for v in array:
         if targetSum - v in seen:
             return [v, (targetSum - v)]
