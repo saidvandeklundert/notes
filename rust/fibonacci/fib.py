c3da23c8aec27dbc71b41f899603ef06de876197
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-13 16:42:27 +0100

    adding

--- rust/fibonacci/fib.py
+++ rust/fibonacci/fib.py
@@ -29,7 +29,7 @@ def python_fib():
 
     elapsed = timer() - start
 
-    print("Python completed in {:.2f} seconds.".format(elapsed))
+    print("Python completed in {:.4f} seconds.".format(elapsed))
 
 
 def rust_fib():
@@ -37,7 +37,7 @@ def rust_fib():
     for number in range(1000):
         rust.get_fibonacci(150)
     elapsed = timer() - start
-    print("Rust completed in {:.2f} seconds.".format(elapsed))
+    print("Rust completed in {:.4f} seconds.".format(elapsed))
 
 
 def run_once():
@@ -46,13 +46,13 @@ def run_once():
     print(rust.get_fibonacci_big(2000))
     elapsed = timer() - start
 
-    print("Rust with completed in {:.2f} seconds.".format(elapsed))
+    print("Rust with completed in {:.4f} seconds.".format(elapsed))
 
     start = timer()
     print(getNthFib(1000))
     elapsed = timer() - start
 
-    print("Python completed in {:.2f} seconds.".format(elapsed))
+    print("Python completed in {:.4f} seconds.".format(elapsed))
 
 
 if __name__ == "__main__":
