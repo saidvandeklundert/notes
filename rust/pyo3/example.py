fd1662aa2f4037239667f0db8a7223e8aadfb2e8
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-13 16:53:11 +0100

    adding

--- rust/pyo3/example.py
+++ rust/pyo3/example.py
@@ -1,28 +1,5 @@
-import pyo3_examples
+import rust
 
 
 if __name__ == "__main__":
-    x = pyo3_examples.sum_as_string(5, 20)
-    print(x)
-    x = pyo3_examples.multiply(5, 20)
-    print(x)
-    print(pyo3_examples.list_sum([10, 10, 10, 10, 10, 5]))
-    words_list = ["apple", "banana", "orange", "pear", "grape"]
-    print(pyo3_examples.list_printer(words_list))
-    array_of_strings = [
-        "apple",
-        "banana",
-        "orange",
-        "pear",
-        "grape",
-        "potato",
-        "avocado",
-        "sushi",
-    ]
-    import datetime
-
-    now = datetime.datetime.now()
-    print(pyo3_examples.array_printer(array_of_strings))
-    duration = datetime.datetime.now() - now
-
-    print(f"Calling Rust took: {duration}")
+    rust.list_printer(["one", "two", "three"])
