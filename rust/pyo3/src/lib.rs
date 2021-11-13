50a7639c4ca0b603a76bf18322aae666304ddb0b
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-13 17:35:22 +0100

    add

--- rust/pyo3/src/lib.rs
+++ rust/pyo3/src/lib.rs
@@ -1,4 +1,7 @@
+extern crate serde;
+extern crate serde_json;
 use pyo3::prelude::*;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 /// Multiply two numbers:
@@ -48,9 +51,22 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 }
 
 #[pyfunction]
-fn human_says_hi(human: String) {
-    println!("{}", human)
+fn human_says_hi(human_data: String) {
+    println!("{}", human_data);
+    let human: Human = serde_json::from_str(human_data).unwrap();
+
+    println!(
+        "Now we can work with the struct: {:?}.\n {} is {} years old.",
+        human, human.name, human.age,
+    )
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Human {
+    name: String,
+    age: u8,
+}
+
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
