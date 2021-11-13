22a515c6f98a4a18b8a4265d02ca10d8512a180b
Said van de Klundert <said.van.de.klundert@ibm.com>
2021-11-13 07:57:13 +0100

    change arg

---
+++ assignments/e_recursion_nth_fibonacci/fib_rust_and_python.py
@@ -0,0 +1,67 @@
+import rust
+
+inputs = [
+    {"number": 1},
+    {"number": 2},
+    {"number": 3},
+    {"number": 4},
+    {"number": 5},
+    {"number": 6},
+    {"number": 7},
+    {"number": 8},
+    {"number": 9},
+    {"number": 10},
+    {"number": 11},
+    {"number": 12},
+    {"number": 13},
+]
+
+
+def getNthFib(number):
+    if number == 1:
+        return 1
+    elif number == 2:
+        return 2
+
+    fib_seq = [0]
+    while number > 0:
+        number -= 1
+        if len(fib_seq) == 1:
+            fib_seq.append(1)
+        else:
+            last = fib_seq[-1]
+            second_to_last = fib_seq[-2]
+            fib_seq.append(last + second_to_last)
+
+    return fib_seq[-1]
+
+
+def getNthFib_a(number):
+    fib_seq = [0, 1]
+    counter = 3
+    while counter <= number:
+        next_fib = fib_seq[-1] + fib_seq[-2]
+        fib_seq[0] = fib_seq[1]
+        fib_seq[1] = next_fib
+        counter += 1
+
+    return fib_seq[1] if number > 1 else fib_seq[0]
+
+
+def getNthFib_rec(number):
+    if number == 2:
+        return 1
+    elif number == 1:
+        return 0
+    else:
+        return getNthFib_rec(number - 1) + getNthFib_rec(number - 2)
+
+
+if __name__ == "__main__":
+    for test in inputs:
+        print(f"input argument: {test}")
+        print("Python:", getNthFib(**test))
+
+        print("Rust:", rust.get_fibonacci(**test))
+
+        print(50 * "-")
