513f6f73c869e490616b52bd9414ba3ca6be37ea
Said van de Klundert <said.van.de.klundert@ibm.com>
2022-01-11 06:33:18 +0100

    functions

--- C/extend_python/python_api/functions/src/c_functions.c
+++ C/extend_python/python_api/functions/src/c_functions.c
@@ -1,6 +1,30 @@
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
+static PyObject *
+spam_system(PyObject *self, PyObject *args)
+{
+    const char *command;
+    int sts;
+
+    if (!PyArg_ParseTuple(args, "s", &command))
+        return NULL;
+    sts = system(command);
+    return PyLong_FromLong(sts);
+}
+
+static PyObject *name_printer(PyObject *self, PyObject *args)
+{
+    char *name;
+    char greeting[255] = "Hello ";
+    if (!PyArg_ParseTuple(args, "s", &name))
+    {
+        return NULL;
+    }
+    strcat(greeting, name);
+    return Py_BuildValue("s", greeting);
+}
+
 // Function definition:
 static PyObject *printer(PyObject *self, PyObject *args)
 {
@@ -11,6 +35,7 @@ static PyObject *printer(PyObject *self, PyObject *args)
 // struct used to describe a method of an extension type:
 static PyMethodDef module_methods[] = {
     {"printer", printer, METH_NOARGS, "Hello from C"},
+    {"system", spam_system, METH_VARARGS, "Execute a shell command."},
     {NULL, NULL, 0, NULL}}; // signal the end of the method definitions
 
 // The PyModuleDef is the module definition struct:
--- C/extend_python/python_api/readme.md
+++ C/extend_python/python_api/readme.md
@@ -1,4 +1,24 @@
 
+# Using the example extensions
+
+
+There are several example extensions in this folder. Here is an example on how to install and run the `functions` example:
+
+```
+root@pyo3:/var/tmp/python/C/extend_python/python_api# pip install -e functions/src/
+```
+
+Most extension modules have an `example.py` and a `test.py` file that you can run:
+
+```
+root@pyo3:/var/tmp/python/C/extend_python/python_api# python3 functions/src/example.py 
+Hello from C
+root@pyo3:/var/tmp/python/C/extend_python/python_api# python3 functions/src/test.py    
+Hello from C
+```
+
+
+
 # making an extension module:
 
 Let's say the package is located in the `src/` directory. If it has the `setup.py` file, then the following will build and install the package:
