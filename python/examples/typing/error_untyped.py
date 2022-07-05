576fe69b9b7ad7c64be05b958f1032303071e239
Said van de Klundert <said.van.de.klundert@gmail.com>
2022-07-05 08:10:25 +0200

    adding

--- python/examples/typing/error_untyped.py
+++ python/examples/typing/error_untyped.py
@@ -6,9 +6,9 @@ class SalesSummary:
 
 
 summary_correct = SalesSummary(
-    sales=10.12, country="USA", product_codes=[123, 12414, 124, 123123, 1231]
+    sales=10, country="USA", product_codes=[123, 12414, 124, 123123, 1231]
 )
 
 summary_wrong = SalesSummary(
-    sales=10, country="USA", product_codes=[123, 124, {"a": "b"}, 1231, "12"]
+    sales=10.0, country="USA", product_codes=[123, 124, {"a": "b"}, 1231, "12"]
 )
